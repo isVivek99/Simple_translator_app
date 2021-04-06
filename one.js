90b3ee5df648ed1641ed919ceb3b0f037b43ddbc
Vivek Lokhande <vivek2811999@gmail.com>
2021-04-06 10:46:00 +0530

    added fetch() to asynchronously call the server

--- one.js
+++ one.js
@@ -1,9 +1,4 @@
-function buttonClick(){
-    console.log("clicked");
-    var inputValue = inputText.value; 
-    console.log("input",inputValue);
-    outputText.innerText=inputValue+" banana language."; 
-}
+
 
 var button = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
@@ -11,7 +6,26 @@ var outputText = document.querySelector("#txt-output");
 
 
 
-
 button.addEventListener("click", buttonClick);
+
+
+function buttonClick(){
+    console.log("clicked");
+    var inputValue = inputText.value; 
+    console.log("input:",inputValue); 
+    var url = "https://api.funtranslations.com/translate/minion.json";
+    fetch(createURL(url))
+    .then(function responseHandler(response){return response.json()})
+    .then(function logJSON(json){outputText.innerText=json.contents.translated})
+     
+}
+
+function createURL(text){
+    var urlText = text+"?"+"text="+inputText.value;
+    return urlText;
+}
+
+
+
 
 
